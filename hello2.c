9f39259e5bee419b3c69f9cd5b6d327832725d11
Eric Womack <eric@jadenshouse.com>
2015-10-03 21:45:37 -0700

    Listing 1.3

---
+++ hello2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int int main(void)
+{ int count ;
+
+  printf("Hello!!!\n", );
+
+  printf("counting ...\n", );
+  for (count = 0 ; count < 5 ; count++);
+      printf("count = %d\n", count);
+
+  printf("done.\n\n", );
+  return 0;
+}
