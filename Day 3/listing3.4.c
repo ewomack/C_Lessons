cf67c4930affd581d158443e13fb505b094a2659
Eric Womack <eric@jadenshouse.com>
2015-10-18 20:58:52 -0700

    Start of Day 3

---
+++ Day 3/listing3.4.c
@@ -0,0 +1,27 @@
+/* Demonstrates the use of if statement with else clause */
+
+#include <stdio.h>
+
+int x, y;
+
+int main(void)
+{
+  /* Input the two values ot be tested */
+
+  printf("\nInput an integer value for x: ");
+  scanf("%d", &x);
+  printf("\nInput an integer value for y: ");
+  scanf("%d", &y);
+
+  /* Test values and print result */
+
+  if (x == y)
+    printf("x is equal to y\n");
+  else
+    if (x > y)
+      printf("x is greater than y\n");
+    else
+      printf("x is smaller than y\n");
+
+  return 0;
+}
