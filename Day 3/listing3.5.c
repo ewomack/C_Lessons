a4184c0fe08640e9b753aa616f2b1d34d5f1b709
Eric Womack <eric@jadenshouse.com>
2015-10-19 19:21:13 -0700

    Listing 3.5

---
+++ Day 3/listing3.5.c
@@ -0,0 +1,18 @@
+/* Demonstrates the evaluation of relational expessions */
+
+#include <stdio.h>
+
+int a;
+
+int main(void)
+{
+  a = (5 == 5);                    /* Evaluates to 1 */
+  printf ("\na = (5 == 5)\na = %d", a);
+
+  a = (5 != 5);                    /* Evaluates to 0 */
+  printf ("\na = (5 != 5)\na = %d", a);
+
+  a = (12 == 12) + (5 != 1);       /* Evaluates to 1 + 1 */
+  printf ("\na = (12 == 12) + (5 != 1)\na = %d\n", a);
+  return 0;
+}
