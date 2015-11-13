0f4befd4bdbe6a490f5b7e081e53676714b829a4
ewomack <eric@jadenshouse.com>
2015-11-12 20:47:42 -0800

    listing rename

---
+++ Day 1/listing1.3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{ int count ;
+
+  printf("Hello!!!\n");
+
+  printf("counting ...\n");
+    for (count = 0 ; count < 5 ; count++)
+    {
+      printf("count = %d\n", count);
+    }
+  printf("done.\n\n");
+  return 0;
+}
+
+/*for (size_t i = 0; i < count; i++) {
+  code
+}*/
